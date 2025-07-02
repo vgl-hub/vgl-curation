ce130356e1e5c4185ddddd6ef3219664004dcfec
Delphine-L <lariviere.delphine@gmail.com>
2025-07-02 14:52:42 -0400

    remove hard paths and add argparser

--- hap_split.py
+++ hap_split.py
@@ -3,10 +3,27 @@
 
 import csv 
 import pandas as pd
+import argparse
+import textwrap
+
+parser = argparse.ArgumentParser(
+                    prog='hap_split.py',
+                    description='Separate the AGP file for two haplotypes into one AGP file for each haplotype. ',
+                    usage='hap_split.py -1 Hap_1/ -2 Hap_2/ -a curated_agp.agp ',
+                    formatter_class=argparse.RawTextHelpFormatter,
+                    epilog=textwrap.dedent('''
+                                           Outputs: 
+                                           - {hap1_prefix}hap.agp: AGP file for Haplotype 1
+                                           - {hap2_prefix}hap.agp: AGP file for Haplotype 2
+                                           '''))
+ 
+parser.add_argument('-a', '--agp', dest="agp", required=True, help='Path to the curated AGP file')
+parser.add_argument('-1', '--hap1', dest="hap1",required=True, help='Output prefix for haplotype 1')  
+parser.add_argument('-2', '--hap2', dest="hap2", required=True, help='Output prefix for haplotype 2') 
+args = parser.parse_args()
 
 
-
-corr="corrected.agp"
+corr=args.agp
 
 header=[]
 agp_lines=[]
@@ -44,13 +61,13 @@ for line in agp_lines:
             H2_lines.append(line)
 
 
-with open ('Hap_1/hap.agp','w',newline='\n') as file1:
+with open (args.hap1+'hap.agp','w',newline='\n') as file1:
     writer=csv.writer(file1,delimiter='\t')
     writer.writerows(header)
     writer.writerows(H1_lines)
 file1.close()
 
-with open ('Hap_2/hap.agp', 'w', newline='\n') as file2:
+with open (args.hap2+'hap.agp', 'w', newline='\n') as file2:
     writer=csv.writer(file2,delimiter='\t')
     writer.writerows(header)
     writer.writerows(H2_lines)
